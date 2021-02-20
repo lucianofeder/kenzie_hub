e037186f3d8e4fcf39cebce482fc3d92ca626887
Luciano Feder <lucianoo.feder@gmail.com>
2021-02-20 12:18:08 -0300

    created page(NotFound) for not found routes

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,23 @@
+import { ContainerError, Img, CardError, Text, Progress } from "./styles";
+import notfound_img from "../../img/notfound_img.svg";
+
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
 const NotFound = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => history.push("/"), 2500);
+  });
   return (
-    <>
-      <div>Not Found</div>
-    </>
+    <ContainerError>
+      <CardError>
+        <Text>Page not found, redirecting...</Text>
+        <Img src={notfound_img} alt="Not found" />
+        <Progress />
+      </CardError>
+    </ContainerError>
   );
 };
 
